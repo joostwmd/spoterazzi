0ded319b4f42808a4bdd8c1254b2a58b136d9b2f
joostwmd <88150245+joostwmd@users.noreply.github.com>
2022-09-01 17:59:00 +0200

    added lines in app.js for deployment

--- app.js
+++ app.js
@@ -28,6 +28,23 @@ app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 const index = require("./routes/index");
 app.use("/", index);
 
+
+
+
+
+
+//deployment
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
