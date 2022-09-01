a74f622ac52f15914c9e64e3d740129d228dde5b
joostwmd <88150245+joostwmd@users.noreply.github.com>
2022-09-01 18:17:23 +0200

    test

--- app.js
+++ app.js
@@ -22,20 +22,19 @@ require("./config")(app);
 const index = require("./routes/index");
 app.use("/", index);
 
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
 
 //deployment
 const path = require('path');
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use((req, res) => {
   // If no routes match, send them the React HTML.
   res.sendFile(__dirname + "/client/build/index.html");
 });
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
 
 module.exports = app;
